96574de37492e8947481439fc5f405772fc5c0b5
aahil <aahilsheikh1711@gmail.com>
2025-03-19 16:44:31 +0530

    fix error

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Heroimg2 from '../components/Heroimg2';
 import PricingCard from '../components/PricingCard';
-import Work from '../components/work';
+import Work from '../components/Work';
 
 const Project = () => {
   return (
